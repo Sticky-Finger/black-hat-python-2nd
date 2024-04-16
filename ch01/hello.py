d40a335f230fa0e08b42b5551be42076aea7752a
np <wpwwanpengwang@163.com>
2024-04-17 03:01:13 +0800

    feat: code ch01

---
+++ ch01/hello.py
@@ -0,0 +1,18 @@
+
+from lxml import etree
+from subprocess import Popen
+
+import argparse
+import os
+
+def get_ip(machine_name):
+    pass
+
+class Scanner:
+    def __init__(self):
+        pass
+
+if __name__ == 'main':
+    scan = Scanner()
+    print('hello')
+
