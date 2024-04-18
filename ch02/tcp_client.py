3774b90fd301ee6785280e525a8441cf527a9af7
niepeng230 <niepeng230@pingan.com.cn>
2024-04-18 15:18:36 +0800

    Feat: ch02 tcp_server

--- ch02/tcp_client.py
+++ ch02/tcp_client.py
@@ -1,7 +1,10 @@
 import socket
 
-target_host = 'www.bing.com.cn'
-target_port = 80
+# target_host = 'www.bing.com.cn'
+# target_port = 80
+
+target_host = '127.0.0.1'
+target_port = 9998
 
 # create a socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
